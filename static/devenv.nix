b6ae2b41f5ae408f6599f3de195831dc1b5bf5ad
Stijn Ruts <git@stijnruts.be>
2025-08-19 07:18:51 +0200

    Static site template

---
+++ static/devenv.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, ... }:
+let
+  DOMAIN = "example.localhost";
+in {
+  certificates = [ DOMAIN ];
+
+  services.caddy = {
+    enable = true;
+    virtualHosts.${DOMAIN} = {
+      extraConfig = ''
+        tls ${config.env.DEVENV_STATE}/mkcert/${DOMAIN}.pem ${config.env.DEVENV_STATE}/mkcert/${DOMAIN}-key.pem
+        root * public
+        file_server
+      '';
+    };
+  };
+
+  # This lets Caddy bind to privileged ports like 80 and 443
+  scripts.caddy-setcap.exec = ''
+    sudo setcap 'cap_net_bind_service=+ep' ${pkgs.caddy}/bin/caddy
+  '';
+}
+
